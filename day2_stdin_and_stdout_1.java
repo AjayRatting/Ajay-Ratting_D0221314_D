164df660aaf2efbfbeed3801cb32f15b6d5f5d62
AjayRatting <100029888+AjayRatting@users.noreply.github.com>
2022-02-22 22:48:40 +0800

    Create day2_stdin_and_stdout_1.java

---
+++ day2_stdin_and_stdout_1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int r = scan.nextInt();
+        int s = scan.nextInt();
+        
+        System.out.println(a);
+        System.out.println(r);
+        System.out.println(s);
+    }
+}
